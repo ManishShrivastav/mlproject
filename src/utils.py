1cde5f2943786a7fa7391c0e70bfba0ee737a12e
Manish Shrivastav <carttoshoppe@gmail.com>
2025-04-01 15:00:41 -0500

    Predict pipeline and Deployment Configuration for AWS

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
-import sys
 import os
+import sys
+
+import numpy as np 
 import pandas as pd
-import numpy as np
-from src.exception import CustomException
 import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
@@ -14,8 +16,8 @@ def save_object(file_path, obj):
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
